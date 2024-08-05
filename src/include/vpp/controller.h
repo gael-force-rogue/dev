364e5450d976ee111c87af51eea8299b6e8eed7b
humanfriend22 <humanfriend22@gmail.com>
2024-08-04 23:01:44 -0700

    controller button api renaming

--- src/include/vpp/controller.h
+++ src/include/vpp/controller.h
@@ -18,20 +18,20 @@ namespace vpp {
         inline float rightY() { return controller.Axis2.position(); };
 
         // Buttons
-        inline bool Up() { return controller.ButtonUp.pressing(); };
-        inline bool Down() { return controller.ButtonDown.pressing(); };
-        inline bool Left() { return controller.ButtonLeft.pressing(); };
-        inline bool Right() { return controller.ButtonRight.pressing(); };
+        inline bool ButtonUp() { return controller.ButtonUp.pressing(); };
+        inline bool ButtonDown() { return controller.ButtonDown.pressing(); };
+        inline bool ButtonLeft() { return controller.ButtonLeft.pressing(); };
+        inline bool ButtonRight() { return controller.ButtonRight.pressing(); };
 
-        inline bool A() { return controller.ButtonA.pressing(); };
-        inline bool B() { return controller.ButtonB.pressing(); };
-        inline bool X() { return controller.ButtonX.pressing(); };
-        inline bool Y() { return controller.ButtonY.pressing(); };
+        inline bool ButtonA() { return controller.ButtonA.pressing(); };
+        inline bool ButtonB() { return controller.ButtonB.pressing(); };
+        inline bool ButtonX() { return controller.ButtonX.pressing(); };
+        inline bool ButtonY() { return controller.ButtonY.pressing(); };
 
-        inline bool L1() { return controller.ButtonL1.pressing(); };
-        inline bool L2() { return controller.ButtonL2.pressing(); };
-        inline bool R1() { return controller.ButtonR1.pressing(); };
-        inline bool R2() { return controller.ButtonR2.pressing(); };
+        inline bool ButtonL1() { return controller.ButtonL1.pressing(); };
+        inline bool ButtonL2() { return controller.ButtonL2.pressing(); };
+        inline bool ButtonR1() { return controller.ButtonR1.pressing(); };
+        inline bool ButtonR2() { return controller.ButtonR2.pressing(); };
 
         inline void vibrate(const char *str) { controller.rumble(str); };
         inline void vibrate(const std::string str) { controller.rumble(str.c_str()); };
